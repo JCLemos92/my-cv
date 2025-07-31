4ed3fad5cd8d30b3073a43745348ef478e7b1491
JCLemos92 <ee10201@fe.up.pt>
2025-07-31 11:08:25 +0100

    Edit configs to correctly use git pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/my-cv">
       <Routes>
         <Route path="/" element={< App />} />
       </Routes>
