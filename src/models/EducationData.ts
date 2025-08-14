5374a01304c18312cd903ab611dc1e1e4140cf0b
JCLemos92 <ee10201@fe.up.pt>
2025-08-14 15:45:51 +0100

    Add certification documents

--- src/models/EducationData.ts
+++ src/models/EducationData.ts
@@ -1,89 +1,89 @@
 export const academicData = [
-	{
-		id: 1,
-		institution: "FEUP - Faculty of Engineering of the University of Porto",
-		logoUrl: "feup.png",
-		courseTitle: "Master in Informatics and Computing Engineering",
-		certificationUrl: "masters_certificate.pdf",
-		year: "2021-2022",
-	},
-	{
-		id: 2,
-		institution: "FEUP - Faculty of Engineering of the University of Porto",
-		logoUrl: "feup.png",
-		courseTitle: "Bachelor in Informatics and Computing Engineering",
-		certificationUrl: "bachelours_certificate.pdf",
-		year: "2015-2021",
-	},
-	{
-		id: 3,
-		institution: "Cambridge English",
-		logoUrl: "cambridge.png",
-		courseTitle: "Fist Certificate English Exam",
-		certificationUrl: null,
-		year: "2008",
-	},
-	{
-		id: 4,
-		institution: "Colegio de Lamego",
-		logoUrl: "cdl.jpg",
-		courseTitle: "High school",
-		certificationUrl: null,
-		year: "2007-2010",
-	},
-]
+  {
+    id: 1,
+    institution: "FEUP - Faculty of Engineering of the University of Porto",
+    logoUrl: "feup.png",
+    courseTitle: "Master in Informatics and Computing Engineering",
+    certificationUrl: "masters_certificate.pdf",
+    year: "2021-2022",
+  },
+  {
+    id: 2,
+    institution: "FEUP - Faculty of Engineering of the University of Porto",
+    logoUrl: "feup.png",
+    courseTitle: "Bachelor in Informatics and Computing Engineering",
+    certificationUrl: "bachelours_certificate.pdf",
+    year: "2015-2021",
+  },
+  {
+    id: 3,
+    institution: "Cambridge English",
+    logoUrl: "cambridge.png",
+    courseTitle: "Fist Certificate English Exam",
+    certificationUrl: "fce_certificate.pdf",
+    year: "2008",
+  },
+  {
+    id: 4,
+    institution: "Colegio de Lamego",
+    logoUrl: "cdl.jpg",
+    courseTitle: "High school",
+    certificationUrl: "hs_certificate.pdf",
+    year: "2007-2010",
+  },
+];
 
 export const certificationData = [
-	{
-		id: 1,
-		institution: "Busuu",
-		logoUrl: "busuu.png",
-		title: "Level C1 in English on Busuu",
-		certificationUrl:
-			"https://api.busuu.com/anon/certificates/53456e44b701d10bb5eb34bd8fbb4577?lang=en",
-		date: "June 26 2025",
-	},
-	{
-		id: 2,
-		institution: "Pluralsight",
-		logoUrl: "pluralsight.jpg",
-		title: "NgRx Fundamentals",
-		certificationUrl: null,
-		date: "Dec 6, 2024",
-	},
-	{
-		id: 3,
-		institution: "Udemy",
-		logoUrl: "udemy.png",
-		title: "Plan and Execute the Perfect Sprints in Agile and Scrum",
-		certificationUrl:
-			"https://udemy-certificate.s3.amazonaws.com/pdf/UC-f687f33e-4e83-47ad-bf6e-0f9a661826c4.pdf",
-		date: "Sep 2023",
-	},
-	{
-		id: 4,
-		institution: "Udemy",
-		logoUrl: "udemy.png",
-		title: "Complete Web & Mobile Designer in 2023: UI/UX, Figma",
-		certificationUrl:
-			"https://udemy-certificate.s3.amazonaws.com/pdf/UC-3605adbc-3c8c-4dc9-8ee8-b58e9ea28aa9.pdf",
-		date: "May 2023",
-	},
-	{
-		id: 5,
-		institution: "Udemy",
-		logoUrl: "udemy.png",
-		title: "Angular - The Complete Guide(2022 Edition)",
-		certificationUrl:
-			"https://udemy-certificate.s3.amazonaws.com/pdf/UC-39798220-d52c-4854-a406-261530c529fa.pdf",
-		date: "May 2022",
-	},
-	{
-		id: 6,
-		institution: "Udemy",
-		logoUrl: "IFB.png",
-		title: "Banking Concepts",
-		certificationUrl: null,
-		date: "Dec 2021",
-	},
-]
+  {
+    id: 1,
+    institution: "Busuu",
+    logoUrl: "busuu.png",
+    title: "Level C1 in English on Busuu",
+    certificationUrl:
+      "https://api.busuu.com/anon/certificates/53456e44b701d10bb5eb34bd8fbb4577?lang=en",
+    date: "June 26 2025",
+  },
+  {
+    id: 2,
+    institution: "Pluralsight",
+    logoUrl: "pluralsight.jpg",
+    title: "NgRx Fundamentals",
+    certificationUrl: null,
+    date: "Dec 6, 2024",
+  },
+  {
+    id: 3,
+    institution: "Udemy",
+    logoUrl: "udemy.png",
+    title: "Plan and Execute the Perfect Sprints in Agile and Scrum",
+    certificationUrl:
+      "https://udemy-certificate.s3.amazonaws.com/pdf/UC-f687f33e-4e83-47ad-bf6e-0f9a661826c4.pdf",
+    date: "Sep 2023",
+  },
+  {
+    id: 4,
+    institution: "Udemy",
+    logoUrl: "udemy.png",
+    title: "Complete Web & Mobile Designer in 2023: UI/UX, Figma",
+    certificationUrl:
+      "https://udemy-certificate.s3.amazonaws.com/pdf/UC-3605adbc-3c8c-4dc9-8ee8-b58e9ea28aa9.pdf",
+    date: "May 2023",
+  },
+  {
+    id: 5,
+    institution: "Udemy",
+    logoUrl: "udemy.png",
+    title: "Angular - The Complete Guide(2022 Edition)",
+    certificationUrl:
+      "https://udemy-certificate.s3.amazonaws.com/pdf/UC-39798220-d52c-4854-a406-261530c529fa.pdf",
+    date: "May 2022",
+  },
+  {
+    id: 6,
+    institution: "Udemy",
+    logoUrl: "IFB.png",
+    title: "Banking Concepts",
+    certificationUrl: null,
+    date: "Dec 2021",
+  },
+];
